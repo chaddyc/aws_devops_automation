08bac80b6086e505a5f8ae849adc9813c2e98ffb
Chad Crouch <34277139+chaddyc@users.noreply.github.com>
2020-08-31 09:17:16 +0200

    Create ami_cleanup_30days.py
    Upload AMI cleanup python script.

---
+++ python/ami_cleanup_30days.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import boto3
+from dateutil.parser import parse
+import datetime
+age = 30
+aws_profile_name = 'prod'
+def days_old(date):
+    get_date_obj = parse(date)
+    date_obj = get_date_obj.replace(tzinfo=None)
+    diff = datetime.datetime.now() - date_obj
+    return diff.days
+
+boto3.setup_default_session(profile_name = aws_profile_name)
+ec2 = boto3.client('ec2')
+amis = ec2.describe_images(Owners=[
+        'self'
+    ])
+for ami in amis['Images']:
+    create_date = ami['CreationDate'] 
+    ami_id = ami['ImageId']
+    # print ami['ImageId'], ami['CreationDate']
+    day_old = days_old(create_date)
+    if day_old > age:
+        print "deleting -> " + ami_id + " - create_date = " + create_date
+        # deregister the AMI
+        ec2.deregister_image(ImageId=ami_id) #comment out this line first to see if all correct images older than 30 days print out as a test
